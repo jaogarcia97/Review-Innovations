2506694516308ff7a14c74f812743cc4ea5ebf44
Jose Jaime Garcia <jose.garcia@mptc.com.ph>
2024-07-31 00:39:58 +0800

    Added seat selection draft

--- Review Innovations/SeatSelection/SeatSelectionView.swift
+++ Review Innovations/SeatSelection/SeatSelectionView.swift
@@ -9,14 +9,56 @@ import SwiftUI
 
 struct SeatSelectionView: View {
     @State private var animateGradient: Bool = false
+    @ObservedObject var viewModel = SeatSelectionViewModel(numberOfSeats: 50)
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 30))
+    ]
     var body: some View {
-        NavigationView{
-            ZStack{
-                MainBackground(animateGradient: $animateGradient)
-                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Select your seat")
+                .font(.largeTitle)
+                .padding()
+            
+            LazyVGrid(columns: columns, spacing: 10) {
+                ForEach(viewModel.seats.indices, id: \.self) { index in
+                    SeatView(isSelected: viewModel.seats[index].isSelected)
+                        .onTapGesture {
+                            viewModel.toggleSeatSelection(at: index)
+                        }
+                }
             }
+            .padding()
         }
     }
+}
+
+struct SeatView: View {
+    let isSelected: Bool
+    
+    var body: some View {
+        Circle()
+            .fill(isSelected ? Color.green : Color.gray)
+            .frame(width: 30, height: 30)
+            .padding(5)
+    }
+}
+
+class SeatSelectionViewModel: ObservableObject {
+    @Published var seats: [Seat]
+
+    init(numberOfSeats: Int) {
+        self.seats = Array(repeating: Seat(), count: numberOfSeats)
+    }
+
+    func toggleSeatSelection(at index: Int) {
+        seats[index].isSelected.toggle()
+    }
+}
+
+struct Seat: Identifiable {
+    let id = UUID()
+    var isSelected: Bool = false
 }
 
 #Preview {
